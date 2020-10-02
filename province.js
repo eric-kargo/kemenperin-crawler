bcea629714a380ff8f8d5452dcfdd327785998c6
royyan-kargo <royyan@kargo.tech>
2020-10-02 13:52:12 +0700

    Add crawled data

---
+++ province.js
@@ -0,0 +1,104 @@
+module.exports = [
+  {
+    51: 'Bali',
+  },
+  {
+    36: 'Banten',
+  },
+  {
+    17: 'Bengkulu',
+  },
+  {
+    34: 'D.I. Yogyakarta',
+  },
+  {
+    31: 'DKI Jakarta',
+  },
+  {
+    75: 'Gorontalo',
+  },
+  {
+    15: 'Jambi',
+  },
+  {
+    32: 'Jawa Barat',
+  },
+  {
+    33: 'Jawa Tengah',
+  },
+  {
+    35: 'Jawa Timur',
+  },
+  {
+    61: 'Kalimantan Barat',
+  },
+  {
+    63: 'Kalimantan Selatan',
+  },
+  {
+    62: 'Kalimantan Tengah',
+  },
+  {
+    64: 'Kalimantan Timur',
+  },
+  {
+    65: 'Kalimantan Utara',
+  },
+  {
+    19: 'Kepulauan Bangka Belitung',
+  },
+  {
+    21: 'Kepulauan Riau',
+  },
+  {
+    18: 'Lampung',
+  },
+  {
+    81: 'Maluku',
+  },
+  {
+    82: 'Maluku Utara',
+  },
+  {
+    11: 'Nanggroe Aceh Darussalam',
+  },
+  {
+    52: 'Nusa Tenggara Barat',
+  },
+  {
+    53: 'Nusa Tenggara Timur',
+  },
+  {
+    92: 'Papua',
+  },
+  {
+    91: 'Papua Barat',
+  },
+  {
+    14: 'Riau',
+  },
+  {
+    76: 'Sulawesi Barat',
+  },
+  {
+    73: 'Sulawesi Selatan',
+  },
+  {
+    72: 'Sulawesi Tengah',
+  },
+  {
+    74: 'Sulawesi Tenggara',
+  },
+  {
+    71: 'Sulawesi Utara',
+  },
+  {
+    13: 'Sumatera Barat',
+  },
+  {
+    16: 'Sumatera Selatan',
+  },
+  {
+    12: 'Sumatera Utara',
+  },
+]
